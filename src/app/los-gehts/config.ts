81937927c1ed1af3d03caf8b479a864e5a8caecf
ypislon <ypislon@freenet.de>
2018-01-09 08:30:03 +0100

    in workshop changes

--- src/app/los-gehts/config.ts
+++ src/app/los-gehts/config.ts
@@ -1,19 +1,16 @@
 'use strict';
 
-export const menu_text = 'Lena Mohr';
-export const menu_logo = '/assets/logo.svg';
+export const menu_text = 'Immanuel';
+export const menu_logo = '/assets/logo2.svg';
 
 export const header_titel = 'Hallo!';
 export const header_untertitel = 'Willkommen beim "Ready To Code?" Workshop.';
 export const header_bild = 'url("/assets/bg.png")';
 
 export const intro_titel = 'Das bin ich!';
-export const intro_text = 'Est ipsum consequatur aut rerum ut. Quo harum voluptas voluptate rem. Animi praesentium quidem eos doloremque molestiae non sint.\n' +
-    '\n' +
-    'Quia ducimus vero ipsum eos quibusdam reiciendis ut. Officiis dolorum blanditiis sapiente fugit id est sit nihil. Excepturi non laboriosam neque rem voluptatem vel. Est hic quia magnam. Deserunt quae qui maiores et asperiores. Voluptatem nihil illum nemo illum et tempora fuga autem.';
-
+export const intro_text = '<i>heyho</i>';
 export const intro_bild = '/assets/IMG_2672.JPG';
 export const intro_bild_untertitel = 'Ein Bild aus meinem Urlaub';
 
-export const quote_text = 'Coden ist gar nicht so schwierig!';
-export const quote_autor = 'Lena Mohr';
+export const quote_text = 'Coden ist überhaupt nicht schwierig!';
+export const quote_autor = 'Manu';
